94c7abdf9c1941cd1ad0ff31c3319df30913337f
Kiril Lazarov <kirillazarov@abv.bg>
2023-06-05 20:22:51 +0300

    Added logic for question mark

--- Course Project/plots.py
+++ Course Project/plots.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def intersection_problem_plot():
+def intersection_problem_plot(question_mark = False):
     bodyA_pos = np.array([-4, -4])
     bodyB_pos_0 = np.array([3, -2])
     bodyB_pos_1 = np.array([2, 2])
@@ -24,21 +24,45 @@ def intersection_problem_plot():
     plt.text(bodyB_pos_0[0] - 0.3, bodyB_pos_0[1] - 0.8, 'B', fontsize=12, fontweight='bold')
     plt.text(bodyB_pos_1[0] - 0.3, bodyB_pos_1[1] - 0.8, 'B', fontsize=12, fontweight='bold')
     plt.text(bodyB_pos_0[0] + 0.4, bodyB_pos_0[1], 'Frame 0', fontsize=8)
-    plt.text(bodyB_pos_1[0] + 0.4, bodyB_pos_1[1], 'Frame 1', fontsize=8)
+    plt.text(bodyB_pos_1[0] - 1.3, bodyB_pos_1[1], 'Frame 1', fontsize=8)
     plt.text(signal_0_pos[0] - 2.5, signal_0_pos[1] + 0.5, 'Gravitational signal', fontsize=8)
     plt.text(signal_1_pos[0] - 2.5, signal_1_pos[1] + 0.5, 'Gravitational signal', fontsize=8)
-    plt.text(2.4, 0, '?', fontsize=20)
-    plt.text(1.5, -0.5, 'Point of intersection', fontsize=8)    
     
-    plt.title('The point-of-intersection problem')
-
     plt.xlim(-5, 5)
     plt.ylim(-5, 5)
 
     plt.xlabel('X')
     plt.ylabel('Y')
+    
+    if not question_mark:
+        plt.text(2.4, 0, '?', fontsize=20)
+        plt.text(1.5, -0.5, 'Point of intersection', fontsize=8) 
+        
+        
+    else:
+        line_body_to_body_x = [bodyB_pos_0[0], bodyB_pos_1[0]]
+        line_body_to_body_y = [bodyB_pos_0[1], bodyB_pos_1[1]]
+        
+        line_signal_to_signal_x = [signal_0_pos[0], signal_1_pos[0]]
+        line_signal_to_singal_y = [signal_0_pos[1], signal_1_pos[1]]       
+        
+        plt.plot(line_body_to_body_x, line_body_to_body_y, color='orange')
+        plt.plot(line_signal_to_signal_x, line_signal_to_singal_y, color='gray')
+        
+        plt.scatter(2.58, -0.35, color='red')
+        
+        plt.text(0, -0.5, 'Point of intersection', fontsize=8)
+        
+    plt.title('The point-of-intersection problem')
 
     plt.plot(lineA_to_frame_0_x, lineA_to_frame_0_y, color='gray')
-    plt.plot(linaA_to_frame_1_x, linaA_to_frame_1_y, color='gray')
-
+    plt.plot(linaA_to_frame_1_x, linaA_to_frame_1_y, color='gray') 
+    
     plt.show()
+    
+
+   
+    
+    
+
+  
