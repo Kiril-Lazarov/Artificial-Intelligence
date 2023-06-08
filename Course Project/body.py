d45b3cb0419b0a5f7a568668a7ea6f85a8da28f6
Kiril Lazarov <kirillazarov@abv.bg>
2023-06-08 15:18:54 +0300

    Add comment to integrate_acceleration method

--- Course Project/body.py
+++ Course Project/body.py
@@ -27,6 +27,7 @@ class Body:
     def distance(point1, point2):
         return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
+    # This method integrates the gravitational acceleration to find the instantaneous velocity.
     @staticmethod
     def integrate_acceleration(M, r):
         return Body.G * M / r 
